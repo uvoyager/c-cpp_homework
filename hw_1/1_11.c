a8ece2dc1b19f3a350a38da5bde37939e1b93eed
Uliana <u.trubach08@gmail.com>
2026-09-11 15:00:30 +0300

    homework 1

---
+++ hw_1/1_11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char w1[32], w2[32];
+    printf("input 1st word: ");
+    scanf("%s", &w1);
+    printf("input 2nd word: ");
+    scanf("%s", &w2);
+    printf("!!! %s, %s !!!\n", w1, w2);
+}
